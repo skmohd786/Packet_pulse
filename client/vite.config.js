74b6e1c68a5e12ac018f0b0d90cb5b4053ec09ae
shaikh mohd <shaikhmohd00009@gmail.com>
2026-08-10 15:01:47 +0530

    added backend deployed URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://packet-pulse-x2o8.vercel.app/',
         changeOrigin: true,
       },
     },
